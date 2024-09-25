8fe883b328b841de8d610ac2fe9885ed79d318b5
rperezpin <123058849+rperezpin@users.noreply.github.com>
2024-09-25 12:20:07 +0200

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,10 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
-
+const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use("/book", require("./router/book.js").routes);
+app.use("/user", require("./router/user.js").routes);
+app.use("/review", require("./router/review.js").routes);
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
-
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
-
-app.use("/customer", customer_routes);
-app.use("/", genl_routes);
-
-app.listen(PORT,()=>console.log("Server is running"));
+app.set("json spaces", 2);
+app.listen(5000, () => console.log("Server is running"));
